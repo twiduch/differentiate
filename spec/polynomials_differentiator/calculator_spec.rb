608e840780143afd229be362c5f079438c1bb866
Tomasz Widuch <twiduch@gmail.com>
2017-03-10 12:15:15 +0000

    expected behaviour tests added

--- spec/polynomials_differentiator/calculator_spec.rb
+++ spec/polynomials_differentiator/calculator_spec.rb
@@ -2,4 +2,27 @@
 require 'spec_helper'
 
 describe Calculator do
+  let(:subject) { Calculator.new }
+
+  describe 'expected behaviour for' do
+    it '3/2/1 is 6x+2' do
+      subject.data = '3/2/1'
+      expect(subject.result).to eq '6x+2'
+    end
+
+    it '4/3/2/1 is 12x^2+6x+2' do
+      subject.data = '4/3/2/1'
+      expect(subject.result).to eq '12x^2+6x+2'
+    end
+
+    it '4/3/0/1 is 12x^2+6x' do
+      subject.data = '4/3/0/1'
+      expect(subject.result).to eq '12x^2+6x'
+    end
+
+    it '4/-5/0/1 is 12x^2-10x' do
+      subject.data = '4/-5/0/1'
+      expect(subject.result).to eq '12x^2-10x'
+    end
+  end
 end
